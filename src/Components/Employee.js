6aa17e0037cc9798b19ecdab4b33e15b742ec2bc
Javier <javiergalarreta44@gmail.com>
2025-01-11 15:00:12 -0300

    update button

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,3 +1,5 @@
+import EditEmployee from "./EditEmployees";
+
 function Employee(props) {
   return (
     <div className="m-2 py-8 px-8 max-w-sm space-y-2 bg-white rounded-xl shadow-lg sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:gap-x-6">
@@ -10,9 +12,7 @@ function Employee(props) {
           <p className="text-lg text-black font-semibold">{props.name}</p>
           <p className="text-slate-500 font-medium">{props.role}</p>
         </div>
-        <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-          Update
-        </button>
+        <EditEmployee />
       </div>
     </div>
   );
